ef2e1e6636fd1e1d7f92831f4e6ad72caee07307
İhsan <altnskihsan@gmail.com>
2023-12-18 21:37:11 +0300

    ihsan

---
+++ src/test/java/tests/ihsan.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class ihsan {
+
+    @Test
+    public void testCase5(){
+
+    }
+    @Test
+    public void testCase7(){
+
+    }
+    @Test
+    public void testCase8(){
+
+    }
+}
