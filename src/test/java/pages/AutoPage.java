b75cae0c96e1642a7ccc49da89f2e66a92594f84
İhsan <altnskihsan@gmail.com>
2023-12-19 16:36:35 +0300

    ihsan.19-12

--- src/test/java/pages/AutoPage.java
+++ src/test/java/pages/AutoPage.java
@@ -1,4 +1,41 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class AutoPage {
+    public AutoPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//div[@class= 'carousel-inner'])[1]")
+    public WebElement anaSayfaSlider;
+
+    @FindBy(xpath = "//a[@href='/login']")
+    public WebElement signupLoginButonu;
+
+    @FindBy(xpath = "//*[text()='Login to your account']")
+    public WebElement loginToYourAccount;
+
+    @FindBy(xpath = "//*[@data-qa='login-email']")
+    public WebElement emailKutusu;
+
+    @FindBy(xpath = "//*[@data-qa='login-password']")
+    public WebElement şifreKutusu;
+
+    @FindBy(xpath = "//*[@data-qa='login-button']")
+    public WebElement loginButonu;
+
+
+    @FindBy(xpath = "//*[text()='Your email or password is incorrect!']")
+    public WebElement loginHataMesajı;
+
+
+
+
+
+
+
 }
