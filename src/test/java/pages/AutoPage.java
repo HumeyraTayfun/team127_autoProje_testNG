ec7215dbc6d7a95a3efe2b30dd9acab8ca565a32
Senaydaakkaya <senaydaakkaya@gmail.com>
2023-12-19 19:49:44 +0300

    senayda.19-12

--- src/test/java/pages/AutoPage.java
+++ src/test/java/pages/AutoPage.java
@@ -1,4 +1,32 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class AutoPage {
+
+    public AutoPage(){
+        PageFactory.initElements(Driver.getDriver(),this);}
+
+    @FindBy(xpath = "//a[@href='/login']")
+    public WebElement signupButonu;
+
+    @FindBy (xpath = "//*[text()='New User Signup!']")
+    public WebElement newUserYaziElementi;
+
+    @FindBy (xpath = "//*[@data-qa='signup-name']")
+    public WebElement nameButton;
+
+    @FindBy (xpath = "//*[@data-qa='signup-email']")
+    public WebElement emailButton;
+
+    @FindBy (xpath = "//*[@data-qa='signup-button']")
+    public WebElement signupButton;
+
+    @FindBy (xpath = "//*[text()='Email Address already exist!']")
+    public WebElement hataMesaji;
+
+
 }
--- src/test/java/tests/senayda.java
+++ src/test/java/tests/senayda.java
@@ -1,12 +1,42 @@
 package tests;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.AutoPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class senayda {
 
     @Test
     public void testCase05 (){
 
+        // 1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        Driver.getDriver().get(ConfigReader.getProperty("autoUrl"));
 
+        //3. Verify that home page is visible successfully
+        String expectedUrl = "https://www.automationexercise.com/";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+
+        //4. Click on 'Signup / Login' button
+        AutoPage autoPage = new AutoPage();
+        autoPage.signupButonu.click();
+
+        //5. Verify 'New User Signup!' is visible
+        Assert.assertTrue(autoPage.newUserYaziElementi.isDisplayed());
+
+        //6. Enter name and already registered email address
+        autoPage.nameButton.sendKeys(ConfigReader.getProperty("usedName"));
+        autoPage.emailButton.sendKeys(ConfigReader.getProperty("usedEmail"));
+
+        //7. Click 'Signup' button
+        autoPage.signupButton.click();
+
+        //8. Verify error 'Email Address already exist!' is visible
+        Assert.assertTrue(autoPage.hataMesaji.isDisplayed());
+
+        Driver.quitDriver();
     }
 }
