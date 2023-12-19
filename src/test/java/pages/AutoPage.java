4115d095f1e13b47637ff6f7d00778585427db22
Murat Koparan <mrtkprn@gmail.com>
2023-12-19 17:07:16 +0300

    murat,19-12

--- src/test/java/pages/AutoPage.java
+++ src/test/java/pages/AutoPage.java
@@ -1,4 +1,28 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class AutoPage {
+
+    public AutoPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//i[@class='fa fa-home']")
+    public WebElement homepagelink;
+
+    @FindBy(xpath = "//i[@class='fa fa-lock']")
+    public WebElement singupbutton;
+
+    @FindBy(xpath = "//li[10]")
+    public WebElement loggedtext;
+
+    @FindBy(xpath = "//a[@href='/delete_account']")
+    public WebElement deleteacc;
+
+    @FindBy(xpath = "//h2[@data-qa='account-deleted']")
+    public  WebElement accdeleted;
 }
